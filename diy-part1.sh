12714026ad3ca13c61143cd29c110a536ba4dd63
myd2898129 <myd2898129@outlook.com>
2023-03-12 07:54:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/myd2898129/luci-app-ddns-go.git package/ddns-go
 git clone https://github.com/myd2898129/openwrt-packages package/kiddin9-packages
