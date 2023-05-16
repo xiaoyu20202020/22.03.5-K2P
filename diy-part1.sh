e125902a372cc78e551e3aa75d5bed355619b1f9
myd2898129 <myd2898129@outlook.com>
2023-05-16 20:34:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
-git clone https://github.com/kiddin9/openwrt-packages/tree/master/luci-app-zerotier package/luci-app-zerotier
+git clone https://github.com/myd2898129/openwrt-packages package/luci-app-zerotier
